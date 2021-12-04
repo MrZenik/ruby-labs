6178a89eedc5f4a43b3c90ffa30e1afbbf7ad6e3
MrZenik <jack.berlav@gmail.com>
2021-12-04 02:01:18 +0200

    lab13.2

--- app/javascript/PayTypeSelector/CheckPayType.jsx
+++ app/javascript/PayTypeSelector/CheckPayType.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+
 class CheckPayType extends React.Component {
     render() {
         return (
             <div>
                 <div className="field">
-                    <label htmlFor="order_routing_number">Routing #</label>
+                    <label htmlFor="order_routing_number">
+                        {I18n.t("orders.form.check_pay_type.routing_number")}
+                    </label>
+
                     <input type="password"
                            name="order[routing_number]"
                            id="order_routing_number" />
                 </div>
                 <div className="field">
-                    <label htmlFor="order_account_number">Account #</label>
+                    <label htmlFor="order_acount_number">
+                        {I18n.t("orders.form.check_pay_type.account_number")}
+                    </label>
+
                     <input type="text"
                            name="order[account_number]"
                            id="order_account_number" />
@@ -19,4 +26,4 @@ class CheckPayType extends React.Component {
         );
     }
 }
-export default CheckPayType;
+export default CheckPayType
